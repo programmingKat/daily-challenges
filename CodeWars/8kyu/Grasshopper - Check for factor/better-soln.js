afc2297f02c05649dbb83aa5e39dce388413b851
Kat Sol <kamichelles@gmail.com>
2026-09-01 22:00:34 -0400

    added grasshopper check for factor

---
+++ CodeWars/8kyu/Grasshopper - Check for factor/better-soln.js
@@ -0,0 +1,4 @@
+function checkForFactor (base, factor) {
+  // i made this one just to practice ternary operators
+  return base%factor==0 ? true : false 
+}
---
+++ CodeWars/8kyu/Grasshopper - Check for factor/naive-soln.js
@@ -0,0 +1,8 @@
+function checkForFactor (base, factor) {
+  if(base%factor==0){
+    return true
+  }
+  else{
+    return false
+  }
+}
